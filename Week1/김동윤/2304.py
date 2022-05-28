e3f40a511b0e200ce705bcfa1eed2f6b99ed5115
myway00 <junior0614@naver.com>
2022-05-28 16:43:40 +0900

    rebrrt

--- Week1/김동윤/2304.py
+++ Week1/김동윤/2304.py
@@ -12,6 +12,7 @@ save.sort() # x축 기준으로 오름차순
 for i in range(len(save)) :
     save[i].append(i)
 
+# 가장 높이가 긴 아이의 인덱스를 찾기
 maxheight=-999
 maxidx=0
 for i in save:
@@ -20,6 +21,7 @@ for i in save:
         maxidx=i[2]
 
 stk.append(save[0])
+
 #전
 for i in range(1,maxidx+1) :
     if stk[-1][1] < save[i][1] : 
@@ -30,23 +32,11 @@ for i in range(1,maxidx+1) :
 res+=save[maxidx][1]
 stk.append(save[maxidx])
 
-
-# if maxidx<n:
-#     for j in range(maxidx+1, n-1) :
-#         if save[j][1] > save[-1][1] :
-#             # print("res ", res)
-#             res+=(save[j][0]-stk[-1][0])*save[j][1]
-#             stk.append(save[j])
-#     res+=((save[-1][0]-stk[-1][0])*save[-1][1])
-
 #후
-stk2=[]
+stk2=[] # 새로운 스택 생성 
 stk2.append(save[-1])
 
-# print("before stk2 ", stk2)
 for j in range(n-2, maxidx-1, -1) : 
-    # print(save[j])
-    # print("res2 ", res)
     if stk2[-1][1] <= save[j][1] : 
         res+=(stk2[-1][0]-save[j][0])*stk2[-1][1]
         stk2.append(save[j])
