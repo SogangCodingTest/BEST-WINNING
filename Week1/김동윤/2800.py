32a2345872db1c01a5bc935762dc21ce7f6af27d
myway00 <junior0614@naver.com>
2022-06-02 21:45:11 +0900

    김동윤 : 1주차 2493번 풀이

--- Week1/김동윤/2800.py
+++ Week1/김동윤/2800.py
@@ -4,20 +4,24 @@ from itertools import combinations
 inp=list(map(str,sys.stdin.readline().rstrip()))
 save=[] #인덱스 순서쌍 저장 
 stk=[] #"(" 저장할 공간 
+
 for i in range(len(inp)) :
-    if inp[i]=="(" :
+    if inp[i]=="(" : #"(" 를 발견하면 스택에 "("의 인덱스를 넣어놨다가 
         stk.append(i)
     elif inp[i]==")" :
-        save.append([stk.pop(),i])
+        save.append([stk.pop(),i]) 
+        #")" 만나면 스택에 들가있는 최신"("와 쌍이므로 
+        # 스택에 있는 인덱스와 얘 인덱스를 저장
 
 res=[] #결과 담을 리스트 
 
 for i in range(1,len(save)+1): #나온 순서쌍 갯수만큼 돌면서 
+    # 조합의 경우를 구해줄거임 
     #세쌍의 괄호 순서쌍이 있다면 
-    # 하나 뽑을 때, 두개 뽑을 때, 세개 뽑을 때,,각 조합의 경우 구하기
+    # 하나 뽑을 때, 두개 뽑을 때, 세개 뽑을 때,,각 조합의 경우 구해주기
     aftercombination= list(combinations(save, i))
     #print(list(aftercombination))
-    for j in aftercombination : # 조합의 경우를 돌면서, 해당 인덱스를 발견한다면 
+    for j in aftercombination : # 각 조합의 경우를 돌면서, 조합 경우에 해당 인덱스를 발견한다면 
         #print(j)
         inpcpy2 = inp.copy()
         #원본 인풋을 복사해놓은 인풋의 값에서 조합 인덱스 해당하는 괄호 없애주기
