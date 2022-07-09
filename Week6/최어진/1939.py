b638c1595c219977bc1ae71288d816bdeab30dee
poodlepoodle <chammal97@naver.com>
2022-07-09 20:01:35 +0900

    최어진 : 6주차 1939번 풀이

--- Week6/최어진/1939.py
+++ Week6/최어진/1939.py
@@ -1,5 +1,5 @@
+# collections.deque 사용
 from collections import deque
-import pprint
 
 # 섬의 개수 N, 다리의 개수 M 입력
 N, M = map(int, input().split())
@@ -8,51 +8,62 @@ N, M = map(int, input().split())
 links = [[] for _ in range(N)]
 
 for _ in range(M):
+    # A 정점부터 B 정점으로의 간선 가중치 weight 입력
     A, B, weight = map(int, input().split())
     
-    # 양방향 연결 관계 및 가중치 추가
+    # 양방향 연결로 가중치 추가
     links[A - 1].append((B - 1, weight))
     links[B - 1].append((A - 1, weight))
 
-start, end = map(int, input().split())
+# 시작 정점 A, 목표 정점 B 입력
+A, B = map(int, input().split())
 
-visited = [0] * N
-visited[start - 1] = 1
+# 중량 weight가 주어졌을 때 물품을 옮길 수 있는지의 여부를 반환하는 bfs() 정의
+def bfs(weight):
+    # 정점 방문 여부 기록 visited 리스트
+    visited = [False] * N
+    
+    # 큐 초기화
+    q = deque()
 
-# 큐 선언 후 시작 섬의 위치 삽입
-q = deque()
-q.append([start - 1, visited.copy(), 10**9])
-
-# bfs를 위한 함수 정의
-def bfs():
-    global end
-
-    answer = -1
+    # 시작 정점에 대한 방문 여부 기록 및 큐에 추가
+    visited[A - 1] = True
+    q.append(A - 1)
 
     # 큐가 비어있지 않으면 계속 탐색
     while len(q) > 0:
-        # 큐에서 맨 앞 요소(현재 위치한 섬, 방문 기록 리스트, 최대 중량)를 pop
-        current, visited, max_weight = q.popleft()        
+        # 큐에서 맨 앞 요소(현재 위치한 섬)을 pop
+        current = q.popleft()
 
-        # 목표 섬에 도달한 한 가지 방법 찾았을 때 -> 그 때까지의 최대 중량 비교해서 갱신
-        if current == end - 1:
-            answer = max_weight if answer < max_weight else answer
-            continue
+        # 목표 섬에 도달했을 때 true 리턴
+        if current == B - 1:
+            return True
 
-        #########################
-        # 목표 섬에 도달하는 과정인 경우
-        #########################
+        # 목표 섬에 도달하지 못했다면
+        for vertex, edge_weight in links[current]:
+            # 현재 정점에서 연결된 정점들로 방문 여부 체크 후 큐에 삽입
+            if not visited[vertex] and weight <= edge_weight:
+                visited[vertex] = True
+                q.append(vertex)
 
-        # 수빈의 현재 위치를 visited에 기록
+    # 탐색을 끝내고 큐가 빈 상태로 True가 리턴되지 않았다면
+    # A 정점부터 B 정점까지 weight를 운반할 수 없다는 의미이므로 False 리턴
+    return False
 
-        for node, weight in links[current]:
-            if visited[node] == 0:
-                new_visited = visited.copy()
-                new_visited[node] = 1
-                q.append([node, new_visited, weight if max_weight > weight else max_weight])
+# 이분 탐색 초기화
+start = 1
+end = 10**9
+answer = -1
 
-    return answer
+while start <= end:
+    middle = (start + end) // 2
 
-answer = bfs()
+    if bfs(middle):
+        # 가장 마지막으로 탐색에 성공한 중량 최대값 갱신
+        answer = middle
+        start = middle + 1
+    else:
+        end = middle - 1
 
+# 정답 출력
 print(answer)
