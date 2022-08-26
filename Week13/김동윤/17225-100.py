b781903b73b172b11423657698bed3d9e49cc667
myway00 <junior0614@naver.com>
2022-08-27 06:46:19 +0900

    김동윤 : 13주차 1389번 풀이

---
+++ Week13/김동윤/17225-100.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+# 구현
+# 자료 구조
+# 우선순위 큐
+# 큐
+a,b,n = map(int, sys.stdin.readline().split())
+order = []
+for i in range(n ) : 
+    t,c,m = sys.stdin.readline().split()
+    for mm in range(int(m)) : 
+        if c  == 'B' : 
+            order.append((int(t)+mm*a,c))
+        else : 
+            order.append((int(t)+mm*a,c))
+
+
+order.sort() # 손님 주문 시간 순 정렬 
+
+slis = []
+jlis = []
+
+for o in range(len(order)) : 
+    if order[o][1]  == 'B' : 
+        slis.append(str(o+1))
+    else : 
+        jlis.append(str(o+1))
+
+print(len(slis))
+print(" ".join(slis))
+print(len(jlis))
+print(" ".join(jlis))
