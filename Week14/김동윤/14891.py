9c66e447475f24181c21b3df5a94bee7203cab8c
myway00 <junior0614@naver.com>
2022-09-04 21:01:36 +0900

    김동윤 : 14주차 14891번 풀이

--- Week14/김동윤/14891.py
+++ Week14/김동윤/14891.py
@@ -13,39 +13,49 @@ def clock(deq) :
 def counter_clock(deq) :
     deq.append(deq.popleft())
 
-def check_주변(num,dir) : 
-    if 0<=num-1<4 :
+def check_주변(num,dir,visit) : 
+    if 0<=num-1<4 and visit[num-1]==0:
         if 톱니[num-1][2] != 톱니[num][6] :
+            visit[num-1]=1
             #방향이 1인 경우는 시계 방향이고, -1인 경우는 반시계 방향
             if dir==-1 :
                 # 나는 반대 방향으로 돔
+                check_주변(num-1, 1, visit)
                 clock(톱니[num-1])
-                check_주변(num-1, 1)
+                
             else : 
+                check_주변(num-1, -1, visit) # 체크 먼저 해주고 그 다음에 바퀴 돌려야한다! 
                 counter_clock(톱니[num-1])
-                check_주변(num-1, -1)
+                
 
-    if 0<=num+1<4 : 
+    if 0<=num+1<4  and visit[num+1]==0:
         if 톱니[num+1][6] != 톱니[num][2] :
+            visit[num+1]=1
             if dir==-1 :
                 # 나는 반대 방향으로 돔
+                
+                check_주변(num+1, 1, visit)
                 clock(톱니[num+1])
-                check_주변(num+1, 1)
+                
             else : 
+                check_주변(num+1, -1, visit)
                 counter_clock(톱니[num+1])
-                check_주변(num+1, -1)
+                
 
 k = int(sys.stdin.readline())
 
 for i in range(k) :
+
     num, dir = map(int, sys.stdin.readline().split())
-    check_주변(num-1, dir)
+    visit = [0 for _ in range(4)]
+    visit[num-1] = 1
+    check_주변(num-1, dir,  visit )
     if dir==-1 :
         counter_clock(톱니[num-1])
     else : 
         clock(톱니[num-1])
-    
-# print(톱니)
+    # print(톱니)
+
 res = 0
 if 톱니[0][0] == 1 : res+=1
 if 톱니[1][0] == 1 : res+=2
