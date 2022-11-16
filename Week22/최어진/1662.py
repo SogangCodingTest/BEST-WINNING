b273bdd22ba4667b482c271b621ae376eaca2096
poodlepoodle <chammal97@naver.com>
2022-11-16 14:25:02 +0900

    최어진 : 22주차 1662번 풀이

--- Week22/최어진/1662.py
+++ Week22/최어진/1662.py
@@ -4,27 +4,27 @@ from collections import deque
 def input(): return sys.stdin.readline().rstrip()
 
 string = input()
-q = deque()
+stack = deque()
 
 for letter in string:
     if letter == ')':
         cnt = 0
-        pull = q.pop()
+        pull = stack.pop()
         while pull != '(':
             if pull[0] == 'x':
                 cnt += int(pull[1:])
             else:
                 cnt += 1
-            pull = q.pop()
+            pull = stack.pop()
 
-        mul = int(q.pop())
-        q.append('x' + str(mul * cnt))
+        mul = int(stack.pop())
+        stack.append('x' + str(mul * cnt))
     else:
-        q.append(letter)
+        stack.append(letter)
 
     # print(q)
 
 cnt = 0
-for letter in q:
+for letter in stack:
     cnt += 1 if letter[0] != 'x' else int(letter[1:])
 print(cnt)
