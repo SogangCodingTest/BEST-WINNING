0bf53f1abc90a3fd046f9303b362a98d69504507
myway00 <junior0614@naver.com>
2023-01-02 01:05:04 +0900

    김동윤 : 1987 번 풀이

--- Week24/11509.py
+++ Week24/11509.py
@@ -10,7 +10,7 @@ for i in range(len(hlis)):
     # 현재 풍선 터뜨릴 화살이 ARROW에 있으면 그거 꺼내쓰고 H-1로 갱신
     if hlis[i] in arrow :
         arrow[arrow.index(hlis[i])]-=1
-    # 없으면 H 의 화살 하나 더 써야한다.
+    # 없으면 H 의 새로운 화살 생성해서 써야한다.
     else :
         cnt+=1
         arrow.append(hlis[i]-1)
