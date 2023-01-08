d869e98be367dc2b418490d01202817f4c050a86
poodlepoodle <chammal97@naver.com>
2023-01-08 21:44:48 +0900

    최어진 : 25주차 1987번 풀이

--- Week25/최어진/1987.py
+++ Week25/최어진/1987.py
@@ -1,5 +1,5 @@
-from collections import deque
 import sys
+sys.setrecursionlimit(10**5)
 
 input = sys.stdin.readline
 
@@ -7,24 +7,17 @@ R, C = map(int, input().rstrip().split())
 
 board = [list(input().rstrip()) for _ in range(R)]
 
-q = deque()
-visited = 1 << (ord(board[0][0]) - ord('A'))
-q.append((0, 0, visited, 1))
-
 moves = ((0, 1), (0, -1), (1, 0), (-1, 0))
 
-answer = -1
-
-while q:
-    r, c, visited, level = q.popleft()
-
+def dfs(r, c, visited, level):
+    global answer
     answer = max(answer, level)
 
     for dr, dc in moves:
         if 0 <= r + dr < R and 0 <= c + dc < C:
             if not (visited & (1 << (ord(board[r + dr][c + dc]) - ord('A')))):
-                q.append((r + dr, c + dc, visited ^ (1 << (ord(board[r + dr][c + dc]) - ord('A'))), level + 1))
+                dfs(r + dr, c + dc, visited ^ (1 << (ord(board[r + dr][c + dc]) - ord('A'))), level + 1)
 
-    # print(q)
-
+answer = -1
+dfs(0, 0, 1 << (ord(board[0][0]) - ord('A')), 1)
 print(answer)
