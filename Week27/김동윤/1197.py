cacbd4b19a381ecc5700047cdcd0e08c65582c1c
myway00 <junior0614@naver.com>
2023-01-24 13:10:33 +0900

    김동윤 28주차 2579번 풀이

--- Week27/김동윤/1197.py
+++ Week27/김동윤/1197.py
@@ -7,7 +7,7 @@ answer = []
 
 for _ in range(e) :
     a,b,c = map(int, sys.stdin.readline().rstrip().split())
-    heapq.heappush(edge, (c,a,b))
+    heapq.heappush(edge, (c,a,b)) # 가장 낮은 가중치의 간선들 
 
 parent = [0 for _ in range(v+1)]
 # parent = [i for i in range(V+1)]
@@ -29,7 +29,10 @@ def union(a,b) :
 
 while edge : 
     now_cost, now_a, now_b = heapq.heappop(edge)
-    if find(now_a)!=find(now_b) : # find 를 했을 때 다르면 사이클은 발생하지 않는다. 
+    # 가장 최소 비용의 간선 꺼내기 & 
+    # 구성하는 두 노드가 싸이클을 이루지 않으면 얘네 둘을 이어주기 
+    if find(now_a)!=find(now_b) : 
+        # find 를 했을 때 다르면 사이클은 발생하지 않는다. 
         union(now_a,now_b)
         answer.append(now_cost)
 
